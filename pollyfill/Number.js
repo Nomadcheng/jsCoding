0b594dcebc9ba0426e907d433f0b1762b62b32c8
Nomadcheng <835657506@qq.com>
2017-11-07 14:15:56 +0800

    polyfill

--- pollyfill/Number.js
+++ pollyfill/Number.js
@@ -10,3 +10,21 @@ if (!Number.isSafeInteger) {
     return Number.isInteger( num ) && Math.abs( num ) <= Number.MAX_SAFE_INTEGER;
   };
 }
+// NUmber.isNaN()
+if(!Number.isNaN) {
+  Number.isNaN = function(n) {
+    return (
+      typeof n === "number" && window.isNaN( n )
+    );
+  };
+}
+if(!Number.isNaN) {
+  Number.isNaN = function(n) {
+    return n !== n;
+  };
+}
+//isNegZero()
+function isNegZero(n) {
+  n = Number(n);
+  return (n === 0) && (1 / n === -Infinity);
+}
