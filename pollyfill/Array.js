4986bd38a55c1e1965dbdc736fa61fc468e425bf
Nomadcheng <835657506@qq.com>
2018-05-24 16:08:45 +0800

    function pollyfill

--- pollyfill/Array.js
+++ pollyfill/Array.js
@@ -13,7 +13,7 @@ function fakeJoin(arr, connector) {
 }
 // Array.from
 const toArrayFrom = (() =>
-  Array.from ? Array.from : obj => [].slice.call()
+  Array.from ? Array.from : obj => [].slice.call(obj)
 )
 // Array.of
 function ArrayOf(){
@@ -25,3 +25,68 @@ const contains = (() =>
     ? (arr, value) => arr.includes(value)
     : (arr, value) => arr.some(el => el === value)
 )();
+// 在数组中查找所有出现的x，并返回一个包含索引的数组
+function findall(a, x) {
+  var results = [],
+      len = a.length;
+      pos = 0;
+  while(pos < len) {
+    pos = a.indexOf(x, pos);
+    if(pos === -1) break;
+    results.push(pos);
+    pos = pos + 1;
+  }
+  return results;
+}
+// ES5 isArray
+var isArray = Function.isArray || function(o) {
+  return typeof o === "object" && Object.prototype.toString.call(o) === "[object Array]"
+}
+/*
+** Array.prototype.map
+*/
+var map = Array.prototype.map
+    ? function(a, f) { return a.map(f); }
+    : function(a, f) {
+      var results = [];
+      for(var i = 0, len = a.length; i < len; i++) {
+        // 跳过undefined
+        if(i in a) results[i] = f.call(null, a[i], i, a);
+      }
+      return results;
+    }
+/*
+** Array.prototype.reduce
+*/
+var reduce = Array.prototype.reduce
+    ? function(a, f, initial) {
+      if(arguments.length > 2)
+          return a.reduce(f, initial); //如果传入了一个初使值
+          else return a.reduce(f);     //否则没有初始值
+    }
+    : function(a, f, initial) {
+      var i = 0, len = a.length, accumulator;
+
+      // 以特定的初始值开始，否则第一个值取自a
+      if(arguments.length > 2) {
+        accumulator = initial;
+      } else {
+        if (len == 0) throw TypeError();
+        while(i < len) {
+          // 跳过undefien和empty
+          if(i in a) {
+            accumulator = a[i++];
+            break;
+          }
+          else i++;
+        }
+        if(i == len) throw TypeError();
+      }
+      // 对数组中剩下的元素依次调用f()
+      while(i < len) {
+        if(i in a)
+            accumulator = f.call(undefined, accumulator, a[i], i, a);
+        i++;
+      }
+      return accumulator;
+    };
---
+++ pollyfill/function.js
@@ -0,0 +1,143 @@
+// 将对象o中名为m()的方法替换为另一个方法
+// 可以在调用原始的方法之前和之后记录日志消息
+function trace(o, m) {
+  var original = o[m];   //在闭包中保存原始方法
+  o[m] = function() {
+    console.log(new Date(), "Entering:",m);
+    var result = original.apply(this, arguments); //调用原始函数
+    console.log(new Date(), "Exiting:", m);
+    return result;
+  }
+}
+var  o = {
+  a: 15,
+  m: function() {
+    console.log(this.a);
+  }
+}
+tarce(o,"m");
+o.m();
+// console
+2018-05-24T05:48:03.905Z 'Entering:' 'm'
+15
+2018-05-24T05:48:03.906Z 'Exiting:' 'm'
+
+/*
+** 返回一个函数，通过它来调用o中的方法f()，传递它的所有的实参
+*/
+function bind(f, o) {
+  if (f.binf) return f.bind(o);
+  else return function() {
+    return f.aaply(o, arrguments);
+  };
+}
+/*
+** ES3版本的Function.bind()方法
+*/
+if(!Function.prototype.bind) {
+  Function.prototype.bind = function(o /*, args*/ ) {
+    // 将this和arguments的值保存至变量中
+    // 以便在后面嵌套的函数中可以使用他们
+    var self = this, boundArgs = arguments;
+    console.log(self);
+    // bind()方法返回值是一个函数
+    return function() {
+      // 创建一个实参列表，将传入bind()的第二个及后续的实参都传入这个函数
+      var args = [], i;
+      for(i = 1; i < boundArgs; i++) args.push(boundArgs[i]);
+      for(i = 1; i < arguments.length; i++) args.push(arguments[i]);
+      // 现在将self作为o的方法来调用，传入这些实参
+      return self.apply(o, args);
+    }
+  }
+}
+// 使用
+var sum = function(x, y, z) {return x + y};
+var g = f.bind(null, 1);
+g(2,3); //6
+
+function f(y,z) {return this.x + y + z};
+var g = f.bind({x: 1}, 2);
+g(3); //6
+/*
+** isFunction()
+*/
+function isFunction(x) {
+  return Object.prototype.toString.call(x) === "[object Function]"
+}
+/*
+** 不完全函数
+** 实现一个工具函数将类数组对象(或对象)转换为真正的数组
+*/
+function array(a, n) {return Array.prototype.slice.call(a, n || 0);}
+// 这个函数的实参传递至左侧
+function partialLeft(f /*, args*/) {
+  var args = arguments;
+  return function() {
+    var a = array(args, 1);  //开始处理外部的第一个args
+    a = a.concat(array(arguments)); //然后增加所有内部的实参
+    return f.apply(this, a); //最后基于这个实参列表调用f
+  }；
+}
+// 这个函数的实参传递至右侧
+function partialRight(f /*, args*/) {
+  var args = arguments;
+  return function() {
+    var a = array(arguments);
+    a = a.concat(array(args, 1));
+    return f.apply(this, a);
+  }
+}
+// 这个函数的实参被用做模版
+// 实参列表中的undefined值都被填充
+function partial(f /*, args*/) {
+  var args = arguments;
+  return function() {
+    var a = array(args, 1); 从外部args开始
+    var i = 0, j = 0;
+    // 遍历args，从内部实参填充undefined
+    for(; i < a.length; i++) {
+      if (a[i] === undefined) a[i] = arguments[j++];
+    }
+    // 现在将剩下的内部实参都追加进去
+    a = a.concat(array(arguments, j));
+    return f.apply(this, a);
+  };
+}
+// 使用
+var f = function(x, y, z) {return x * (y - z);};
+// 注意这三个不完全调用的区别
+partialLeft(f, 2)(3, 4) //2*(3-4)
+partialRight(f, 2)(3, 4)//3*(4-2)
+partial(f, undefined, 2)(3, 4)//3*(2-4)
+/*
+** memorize,将上次计算的结果缓存起来，
+** 本质上是牺牲算法的空间复杂度以换取更优的时间复杂度
+** 在客户端编程中代码的执行时间复杂度往往是瓶颈
+** 返回f()的带有记忆功能的版本
+** 只有当f()的实参的字符串表示都不相同时它才会工作
+*/
+function memorize(f) {
+  var cache = {}; //将值保存在闭包内
+  return function() {
+    // 将实参转换成字符串形式，并将其用做缓存的键
+    var key = arguments.length + Array.prototype.join.call(arguments, ",");
+    if(key in cache) return cache[key];
+    else return cache[key] = f.apply(this, arguments);
+  }
+}
+// 展示如何使用缓存,返回两个整数的最大公约数
+function gcd(a, b) {
+  var t;
+  if (a < b) t = b, b = a, a = t;
+  while(b != 0) t = b, b = a % b, a = t;
+  return a;
+}
+var gcdmemo = memorize(gcd);
+gcdmemo(32748378427149234798324, 324923849238493824092384);
+// 当我们写一个递归函数时，往往需要实现记忆功能
+// 我们更希望调用实现了记忆功能的递归函数，而不是原递归函数
+var factorial = memorize(function(n) {
+  return (n <= 1) ? 1: n * factorial(n-1);
+});
+factorial(5); //对于1～4的值也缓存了
